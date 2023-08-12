bc84737f9ae29d7414266390eade27755834961d
Sayed Amman Akhtar <71068761+amman8@users.noreply.github.com>
2023-08-12 17:07:37 +0530

    Create DoorHandler.cs
    Door Code

---
+++ DoorHandler.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllDores : MonoBehaviour
+{
+
+    public Quaternion startQuaternion;
+    //public Vector3 quaternionToVector;
+    public Vector3 reversequaternion;
+    public float Lerptime = 1f;
+    public bool playerneerthedoor = false;
+    [SerializeField] double door_Limit = 0.65;
+    [SerializeField] AudioSource DourSound;
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player")&& gameObject.transform.rotation.y < door_Limit)
+        {
+            Debug.Log("Open");
+            playerneerthedoor = true;
+            DourSound.Play();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Close");
+            playerneerthedoor = false;
+            DourSound.Pause();
+        }
+    }
+
+    void Start()
+    {
+        startQuaternion = transform.rotation;   
+    }
+
+    
+    void Update()
+    {
+        if (playerneerthedoor == true)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(reversequaternion), Time.deltaTime * Lerptime);
+            //Debug.Log(gameObject.transform.rotation.y);
+        }
+        
+    }
+
+    public void snapRotation()
+    {
+        transform.rotation = startQuaternion;
+    }
+}
