c3ded3e781207e502eca31f363906ec40dda3a8d
Sayed Amman Akhtar <71068761+amman8@users.noreply.github.com>
2023-08-25 15:16:34 +0530

    Update CombineObjectsWithSameMaterial.cs
    UI Update
    Bug Fix

--- CombineObjectsWithSameMaterial.cs
+++ CombineObjectsWithSameMaterial.cs
@@ -1,11 +1,15 @@
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 using System.Linq;
 
 public class CombineObjectsWithSameMaterial : EditorWindow
 {
     private string combinedObjectName = "CombinedObject";
+    private Material targetMaterial;
+    private bool materialFoldout = true;
+    private bool transformFoldout = true;
 
     [MenuItem("Window/Combine Objects")]
     public static void ShowWindow()
@@ -17,85 +21,127 @@ public class CombineObjectsWithSameMaterial : EditorWindow
     {
         GUILayout.Label("Combine Objects with Same Material", EditorStyles.boldLabel);
 
-        combinedObjectName = EditorGUILayout.TextField("Combined Object Name:", combinedObjectName);
+        EditorGUILayout.Space();
 
-        if (GUILayout.Button("Combine"))
+        DrawSeparator();
+
+        // Material Section
+        materialFoldout = EditorGUILayout.BeginFoldoutHeaderGroup(materialFoldout, "Material");
+        if (materialFoldout)
         {
-            CombineObjects();
+            targetMaterial = EditorGUILayout.ObjectField("Target Material:", targetMaterial, typeof(Material), false) as Material;
         }
+        EditorGUILayout.EndFoldoutHeaderGroup();
+
+        DrawSeparator();
+
+        // Transform Section
+        transformFoldout = EditorGUILayout.BeginFoldoutHeaderGroup(transformFoldout, "Transform");
+        if (transformFoldout)
+        {
+            combinedObjectName = EditorGUILayout.TextField("Combined Object Name:", combinedObjectName);
+        }
+        EditorGUILayout.EndFoldoutHeaderGroup();
+
+        DrawSeparator();
+
+        EditorGUILayout.Space();
+
+        GUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+
+        if (GUILayout.Button(new GUIContent("Combine All Objects", "Combine all objects with the same material"), GUILayout.Width(200)))
+        {
+            CombineObjects(null);
+        }
+
+        GUILayout.FlexibleSpace();
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(10);
+
+        GUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+
+        if (GUILayout.Button(new GUIContent("Combine Objects with Target Material", "Combine objects with the target material"), GUILayout.Width(200)))
+        {
+            CombineObjects(targetMaterial);
+        }
+
+        GUILayout.FlexibleSpace();
+        GUILayout.EndHorizontal();
     }
 
-    private void CombineObjects()
+
+    private void CombineObjects(Material materialFilter)
     {
-        Material lastMaterial = null;
-        CombineInstance[] combineInstances = new CombineInstance[0];
-        int combinedCount = 0;
+        Scene currentScene = SceneManager.GetActiveScene();
+        Dictionary<Material, List<MeshFilter>> materialToObjectMap = new Dictionary<Material, List<MeshFilter>>();
 
-        GameObject[] staticMeshObjects = FindStaticMeshObjects();
-
-        foreach (GameObject meshObject in staticMeshObjects)
+        foreach (MeshFilter meshFilter in FindStaticMeshFilters())
         {
-            MeshFilter meshFilter = meshObject.GetComponent<MeshFilter>();
-            MeshRenderer meshRenderer = meshObject.GetComponent<MeshRenderer>();
+            MeshRenderer meshRenderer = meshFilter.GetComponent<MeshRenderer>();
 
-            if (meshFilter == null || meshRenderer == null)
+            if (meshRenderer == null)
                 continue;
 
-            if (lastMaterial == null || lastMaterial != meshRenderer.sharedMaterial)
-            {
-                if (lastMaterial != null)
-                {
-                    GameObject combinedObject = CreateCombinedObject(combineInstances, lastMaterial, combinedCount);
-                    DeleteObjects(staticMeshObjects);
-                    combinedObject.name = combinedObjectName + combinedCount;
-                    Selection.activeGameObject = combinedObject;
-                    return;
-                }
+            Material material = meshRenderer.sharedMaterial;
 
-                lastMaterial = meshRenderer.sharedMaterial;
-                combineInstances = new CombineInstance[staticMeshObjects.Length];
+            if (materialFilter != null && material != materialFilter)
+                continue;
+
+            if (!materialToObjectMap.ContainsKey(material))
+            {
+                materialToObjectMap[material] = new List<MeshFilter>();
             }
 
-            CombineInstance combineInstance = new CombineInstance();
-            combineInstance.mesh = meshFilter.sharedMesh;
-            combineInstance.transform = meshFilter.transform.localToWorldMatrix;
-            combineInstances[combinedCount] = combineInstance;
-            combinedCount++;
+            materialToObjectMap[material].Add(meshFilter);
         }
 
-        if (lastMaterial != null)
+        int combinedCount = 0;
+        foreach (KeyValuePair<Material, List<MeshFilter>> pair in materialToObjectMap)
         {
-            GameObject combinedObject = CreateCombinedObject(combineInstances, lastMaterial, combinedCount);
-            DeleteObjects(staticMeshObjects);
+            GameObject combinedObject = CreateCombinedObject(pair.Value.ToArray(), pair.Key, combinedCount);
+            DeleteObjects(pair.Value.Select(meshFilter => meshFilter.gameObject).ToArray());
             combinedObject.name = combinedObjectName + combinedCount;
             Selection.activeGameObject = combinedObject;
+            combinedCount++;
         }
     }
 
-    private GameObject[] FindStaticMeshObjects()
+    private MeshFilter[] FindStaticMeshFilters()
     {
         Scene currentScene = SceneManager.GetActiveScene();
         GameObject[] rootObjects = currentScene.GetRootGameObjects();
-        System.Collections.Generic.List<GameObject> staticMeshObjects = new System.Collections.Generic.List<GameObject>();
+        List<MeshFilter> staticMeshFilters = new List<MeshFilter>();
 
         foreach (GameObject rootObject in rootObjects)
         {
-            staticMeshObjects.AddRange(rootObject.GetComponentsInChildren<MeshFilter>(true)
-                .Where(meshFilter => meshFilter.gameObject.isStatic)
-                .Select(meshFilter => meshFilter.gameObject));
+            staticMeshFilters.AddRange(rootObject.GetComponentsInChildren<MeshFilter>(true)
+                .Where(meshFilter => meshFilter.gameObject.isStatic));
         }
 
-        return staticMeshObjects.ToArray();
+        return staticMeshFilters.ToArray();
     }
 
-    private GameObject CreateCombinedObject(CombineInstance[] combineInstances, Material material, int combinedCount)
+    private GameObject CreateCombinedObject(MeshFilter[] meshFilters, Material material, int combinedCount)
     {
         GameObject combinedObject = new GameObject(combinedObjectName + combinedCount);
         MeshFilter meshFilterCombined = combinedObject.AddComponent<MeshFilter>();
         meshFilterCombined.sharedMesh = new Mesh();
+
+        CombineInstance[] combineInstances = new CombineInstance[meshFilters.Length];
+        for (int i = 0; i < meshFilters.Length; i++)
+        {
+            combineInstances[i].mesh = meshFilters[i].sharedMesh;
+            combineInstances[i].transform = meshFilters[i].transform.localToWorldMatrix;
+        }
+
         meshFilterCombined.sharedMesh.CombineMeshes(combineInstances, true);
+
         MeshRenderer meshRendererCombined = combinedObject.AddComponent<MeshRenderer>();
         meshRendererCombined.sharedMaterial = material;
+
         return combinedObject;
     }
 
@@ -105,5 +151,9 @@ public class CombineObjectsWithSameMaterial : EditorWindow
         {
             DestroyImmediate(obj);
         }
+    }
+    private void DrawSeparator()
+    {
+        GUILayout.Box("", GUILayout.ExpandWidth(true), GUILayout.Height(1));
     }
 }
